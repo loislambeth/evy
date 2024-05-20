e704d6169d22b9a9069a61e0569b2010c3623935
Julia Ogris <julia.ogris@gmail.com>
2024-05-20 20:29:44 +1000

    builtins: Add variadic assert command
    Add variadic assert command such that all of the following are passing assertions
    
    	assert true
    	assert 1==1
    	assert 1 1
    	assert "abc" "abc" "comparing words"
    
    Implement it as builtin in the evaluator package so that it can be used via an
    `evy test` command in a follow up commit.
    
    Add test reporting at the end of the program evaluation, if TestInfo.Reporting
    isn't set to NoReporting and there are calls to assert.
    
    Document it all.
    
    Add examples/human-eval as initial conformance test suite to the Makefile
    under make target `conform`.

---
+++ pkg/evaluator/assertinfo.go
@@ -0,0 +1,53 @@
+package evaluator
+
+import "strconv"
+
+// AssertInfo contains flags for test runs, e.g. FailFast and testResult
+// information, e.g. total count.
+type AssertInfo struct {
+	FailFast           bool
+	NoAssertionSummary bool
+
+	errors []error
+	total  int
+}
+
+// FailCount returns the number of failed assertions.
+func (a *AssertInfo) FailCount() int {
+	return len(a.errors)
+}
+
+// SuccessCount returns the number of successful assertions.
+func (a *AssertInfo) SuccessCount() int {
+	return a.total - len(a.errors)
+}
+
+// TotalCount returns the total number of assertions executed.
+func (a *AssertInfo) TotalCount() int {
+	return a.total
+}
+
+// Report prints a summary of the test results.
+func (a *AssertInfo) Report(printFn func(string)) {
+	if a.NoAssertionSummary || a.total == 0 {
+		return
+	}
+	succs := a.SuccessCount()
+	fails := a.FailCount()
+	var msg string
+	switch {
+	case fails > 0:
+		msg = "❌ " + strconv.Itoa(fails) + " failed assertion" + suffix(fails) + "\n" +
+			"✔️ " + strconv.Itoa(succs) + " passed assertion" + suffix(succs) + "\n"
+	default:
+		msg = "✅ " + strconv.Itoa(succs) + " passed assertion" + suffix(succs) + "\n"
+	}
+	printFn(msg)
+}
+
+func suffix(n int) string {
+	if n == 1 {
+		return ""
+	}
+	return "s"
+}
